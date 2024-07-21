7a495e1d94f2393ab53fcfd48761bb9179841495
Matthew <mattswest01@gmail.com>
2024-07-21 00:55:19 -0700

    Basic Functionality & Pages
    - Working tab switch using frames ( Showing & Hiding )
    - JS Printer to print text to the screen in a retro fashion
    - Cleaning of script.js file
    - Cleaning of html files
    - test python program for download link

--- Modules/Scripts/printer.js
+++ Modules/Scripts/printer.js
@@ -1,23 +1,69 @@
+let homeText = [
+    "INFO",
+    "Westapp is a site for all the projects made by Matthew Weston. All apps are meant to be an escape from modern bloated software",
+    "• No AI",
+    "• Lightweight",
+    "• Open Source <3 ",
+    "• No Internet needed",
+    "• Customizable themes",
+    "• Cross Platform (Windows, Mac, Linux)",
+    "Latest Updates",
+    "About / Contact",
+    "mattswest01@gmail.com"
+]
+let homeIds = [
+    "h_title1",
+    "h_text1",
+    "h_li1",
+    "h_li2",
+    "h_li3",
+    "h_li4",
+    "h_li5",
+    "h_li6",
+    "h_title2",
+    "h_title3",
+    "h_aboutText1"
+]
+
+let varArray = new Array;
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    for (_value in homeIds) {
+        varArray.push(document.getElementById(`${homeIds[_value]}`));
+    }
+    textTyper()
+});
 
-    textTyper('h_title', 'Welcome to my website!');
 
-    function textTyper(id, value) {
-        let currIndex = 0;
-        const textToDisplay = value;
-        const textContainer = document.getElementById(id)
+function textTyper() {
+    let charIndex = 0;
+    let arrIndex = 0;
+    let textToDisplay = homeText[arrIndex];
+    let textContainer = varArray[arrIndex];
 
-        function typeText() {
-            if (currIndex < textToDisplay.length) {
-                textContainer.textContent += textToDisplay.charAt(currIndex);
-                currIndex++;
-                let tempTimeout = Math.floor(Math.random() * 100)
-                console.log(tempTimeout);
-                setTimeout(typeText, tempTimeout);
-            } else {
-                currIndex = 0;
+    function typeText() {
+        if (textContainer == null) {
+            console.log("NULL");
+            textContainer = varArray[0];
+            return;
+        }
+        if (charIndex == 0) {
+            textContainer.style.display = 'block';
+        }
+        if (charIndex < textToDisplay.length) {
+            textContainer.textContent += textToDisplay.charAt(charIndex);
+            charIndex++;
+            setTimeout(typeText, Math.floor(Math.random() * 100));
+        } else {
+            arrIndex++;
+            charIndex = 0;
+            if (arrIndex < varArray.length) {
+                textToDisplay = homeText[arrIndex];
+                textContainer = varArray[arrIndex];
+                typeText();
             }
         }
-        typeText();
     }
-});
+    typeText();    
+};
--- Modules/Scripts/script.js
+++ Modules/Scripts/script.js
@@ -1,32 +1,56 @@
+let timeLabel = "";
+
 document.addEventListener('DOMContentLoaded', function() {
+    changeWindowFrame('cFrameHome', 'navHome');
+    timeLabel = document.getElementById('timeClock');
+    updateDateTime();
+
+    //const inputField = document.getElementById('inputField');
     
-    const inputField = document.getElementById('inputField');
+
     document.addEventListener('keydown', (event) => {
         if (event.ctrlKey && event.key == " ") {
             inputField.focus();
         }
     });
+
+    /*
+    inputField.addEventListener('keydown', (event) => {
+        checkCommandInput(event); 
+    });
+
     inputField.addEventListener('keydown', (event) => {
         if (event.key == "Enter") {
             runCommand();
         }
-    });
+    });*/
+
+    setInterval(updateDateTime, 1000);
 });
+
+
+function updateDateTime() {
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
+    timeLabel.textContent = currentDateTime;
+}
 
 
 function runCommand() {
     const inputFieldValue = document.getElementById('inputField').value;
-
     //! CHECK FOR INVALID INPUT
-
+    //? Create a better setup
     if (inputFieldValue == "help") {
-        console.log("COMMANDS: \n1. HELP");
+        let commandList = 'help\n(ref)resh\ninfo\nexit'
+        alert(`COMMAND LIST\n${commandList}`)
     }
     else if (inputFieldValue == "ref" || inputFieldValue == "refresh") {
         location.reload();
     }
-    else if (inputFieldValue == "install") {
-        alert("cannot install program");
+    else if (inputFieldValue == "info") {
+        const screenWidth = screen.width;
+        const screenHeight = screen.height;
+        alert(`INFO: ${screenWidth}x${screenHeight}`);
     }
     else {
         alert("invalid messag");
@@ -34,7 +58,14 @@ function runCommand() {
 
 }
 
-function changeWindow(path, name) {
+function checkCommandInput(event) {
+    alert("Checking for valid input");
+}
+
+
+
+function changeWindowFrame(uID, name) {
+
     var navBtn1 = document.getElementById('navHome');
     var navBtn2 = document.getElementById('navSoftwareDev');
     var navBtn3 = document.getElementById('navGameDev');
@@ -42,10 +73,14 @@ function changeWindow(path, name) {
     navBtn1.classList.remove("active");
     navBtn2.classList.remove("active");
     navBtn3.classList.remove("active");
-
     var tempElement = document.getElementById(name);
+    tempElement.classList.add("active");
 
-    tempElement.classList.add("active")
 
-    document.getElementById("contentFrame").src = path;
+    document.getElementById("cFrameHome").style.display = "none";
+    document.getElementById("cFrameSoftware").style.display = "none";
+    document.getElementById("cFrameGames").style.display = "none";
+
+    console.log(uID);
+    document.getElementById(uID).style.display = "block";
 }
